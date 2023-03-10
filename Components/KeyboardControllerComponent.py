f0b426fed5a576c64f2dc6e5cf4a4d3f16f3d4dc
Sean Paget <codeMonkey760@gmail.com>
2023-03-10 15:16:48 -0800

    Fix KeyboardControllerComponent

--- Components/KeyboardControllerComponent.py
+++ Components/KeyboardControllerComponent.py
@@ -11,13 +11,13 @@ class KeyboardControllerComponent(py3dengine.Component):
     def update(self, dt):
         x = 0
         z = 0
-        if py3dinput.is_key_pressed('a'):
+        if py3dinput.is_key_pressed(65):
             x = x - 1
-        if py3dinput.is_key_pressed('d'):
+        if py3dinput.is_key_pressed(68):
             x = x + 1
-        if py3dinput.is_key_pressed('s'):
+        if py3dinput.is_key_pressed(83):
             z = z - 1
-        if py3dinput.is_key_pressed('w'):
+        if py3dinput.is_key_pressed(87):
             z = z + 1
 
         displacement = py3dmath.Vector3(x, 0, z)
