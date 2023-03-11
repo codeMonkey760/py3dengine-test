4221647d61fe2e05e752a5e9bfc293df71c974b7
Sean Paget <codeMonkey760@gmail.com>
2023-03-11 09:29:21 -0800

    Implement MouseControllerComponent

--- Components/MouseControllerComponent.py
+++ Components/MouseControllerComponent.py
@@ -1,16 +1,37 @@
 import py3dengine
 import py3dinput
-import py3dlogger
+from py3dmath import Quaternion, Vector3
 
 
 class MouseControllerComponent(py3dengine.Component):
     def __init__(self):
         super().__init__(self)
+        self.x_sens = 1.0
+        self.y_sens = 1.0
+        self.last_pos = None
 
     def update(self, dt):
-        x, y = py3dinput.get_cursor_pos()
+        cur_pos = py3dinput.get_cursor_pos()
 
-        py3dlogger.info('[MouseController]: Cursor is at {},{}'.format(x,y))
+        if self.last_pos is None:
+            self.last_pos = cur_pos
+            return
+
+        delta = (cur_pos[0] - self.last_pos[0], cur_pos[1] - self.last_pos[1])
+        self.last_pos = cur_pos
+
+        try:
+            transform = self.get_owner().get_transform()
+        except AttributeError:
+            transform = None
+        if transform is None:
+            return
+
+        pitch = Quaternion.FromAxisAndDegrees(Vector3(1.0, 0.0, 0.0), delta[1] * self.y_sens)
+        yaw = Quaternion.FromAxisAndDegrees(Vector3(0.0, 1.0, 0.0), delta[0] * self.x_sens)
+
+        transform.set_orientation((pitch * yaw))
 
     def parse(self, values, resource_manager):
-        pass
+        self.x_sens = values['x_sensitivity']
+        self.y_sens = values['y_sensitivity']
