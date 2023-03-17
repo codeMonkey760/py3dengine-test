efa7b5293f6845bba5e4bc848872f9abaf1857e0
Sean Paget <codeMonkey760@gmail.com>
2023-03-16 17:30:42 -0700

    Add space and c keys to control height

--- Components/KeyboardControllerComponent.py
+++ Components/KeyboardControllerComponent.py
@@ -10,17 +10,22 @@ class KeyboardControllerComponent(py3dengine.Component):
 
     def update(self, dt):
         x = 0
+        y = 0
         z = 0
         if py3dinput.is_key_pressed(65):
             x = x - 1
         if py3dinput.is_key_pressed(68):
             x = x + 1
+        if py3dinput.is_key_pressed(67):
+            y = y - 1
+        if py3dinput.is_key_pressed(32):
+            y = y + 1
         if py3dinput.is_key_pressed(83):
             z = z - 1
         if py3dinput.is_key_pressed(87):
             z = z + 1
 
-        displacement = py3dmath.Vector3(x, 0, z)
+        displacement = py3dmath.Vector3(x, y, z)
         try:
             displacement = displacement.normalize()
         except ZeroDivisionError:
