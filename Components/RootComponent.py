03dc8d27b0cddb1fe65f60a947bb6af8cbb05eb4
Sean Paget <codeMonkey760@gmail.com>
2023-05-20 12:18:45 -0700

    Refactor root component to use start and end events

--- Components/RootComponent.py
+++ Components/RootComponent.py
@@ -1,5 +1,6 @@
 import py3dengine
 import py3dinput
+import py3dlogger
 from KeyboardControllerComponent import KeyboardControllerComponent
 
 
@@ -14,25 +15,23 @@ class RootComponent(py3dengine.Component):
         py3dinput.set_key_callback(self.on_1_released, 49, 0, 0)
         py3dinput.set_key_callback(self.on_2_released, 50, 0, 0)
 
-    def on_q_released(self):
+    def start(self):
+        super().start()
+        py3dlogger.info("[RootComponent]: Root says hi")
+        self.camera = self.get_owner().get_child_by_name("Camera").get_component_by_type(KeyboardControllerComponent)
+        self.hammer = self.get_owner().get_child_by_name("Hammer").get_component_by_type(KeyboardControllerComponent)
+
+    def end(self):
+        super().end()
+        py3dlogger.info("[RootComponent]: Root says bye")
+
+    def on_q_released():
         py3dengine.quit()
 
     def on_1_released(self):
-        self.__get_camera()
-        self.__get_hammer()
         self.camera.enable(True)
         self.hammer.enable(False)
 
     def on_2_released(self):
-        self.__get_camera()
-        self.__get_hammer()
         self.camera.enable(False)
         self.hammer.enable(True)
-
-    def __get_camera(self):
-        if self.camera is None:
-            self.camera = self.get_owner().get_child_by_name("Camera").get_component_by_type(KeyboardControllerComponent)
-
-    def __get_hammer(self):
-        if self.hammer is None:
-            self.hammer = self.get_owner().get_child_by_name("Hammer").get_component_by_type(KeyboardControllerComponent)
