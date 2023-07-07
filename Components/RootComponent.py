eb35cf6f5f61ad8c775843ab2a24f6fcd2533e1c
Sean Paget <codeMonkey760@gmail.com>
2023-07-07 10:07:02 -0700

    Add handler for 3 key to root component that activates scene 2

--- Components/RootComponent.py
+++ Components/RootComponent.py
@@ -18,6 +18,7 @@ class RootComponent(py3dengine.Component):
         scene.set_key_callback(self.on_q_released, 81, 0, 0)
         scene.set_key_callback(self.on_1_released, 49, 0, 0)
         scene.set_key_callback(self.on_2_released, 50, 0, 0)
+        scene.set_key_callback(self.on_3_released, 51, 0, 0)
         self.camera = self.get_owner().get_child_by_name("Camera").get_component_by_type(KeyboardControllerComponent)
         self.hammer = self.get_owner().get_child_by_name("Hammer").get_component_by_type(KeyboardControllerComponent)
 
@@ -35,3 +36,9 @@ class RootComponent(py3dengine.Component):
     def on_2_released(self):
         self.camera.enable(False)
         self.hammer.enable(True)
+
+    def on_3_released(self):
+        try:
+            py3dengine.activate_scene("Scene2")
+        except py3dengine.SceneActivationException as err:
+            py3dlogger.warning("Could not activate Scene2 " + str(err))
