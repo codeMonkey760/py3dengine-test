661885760e0a80366c167148cda90ceb2dd76b77
Sean Paget <codeMonkey760@gmail.com>
2025-02-16 11:45:58 -0800

    Fix StrobeLightComponent by rewriting timing and state transitions

--- Components/StrobeLightComponent.py
+++ Components/StrobeLightComponent.py
@@ -16,29 +16,29 @@ class StrobeLightComponent(py3dengine.Component):
     def update(self, dt):
         self.time = self.time + dt
 
-        if 0.0 < self.time <= 1.0 and self.state != 0:
-            self.state_transition(0)
-        elif 1.0 < self.time <= 2.0 and self.state != 1:
-            self.state_transition(1)
-        elif 2.0 < self.time <= 3.0 and self.state != 2:
-            self.state_transition(2)
-        elif 3.0 < self.time <= 4.0 and self.state != 3:
-            self.state_transition(3)
-        else:
-            while self.time > 4.0:
-                self.time = self.time - 4
-            self.state_transition(0)
+        if self.time > 1.0:
+            while self.time > 1.0:
+                self.time -= 1.0
+            self.state_transition((self.state + 1) % 4)
 
     def state_transition(self, new_state):
         if new_state == 0:
-            self.light_component.set_diffuse_color([1.0, 0.0, 0.0])
+            self.light_component.set_diffuse_color([0.7, 0.3, 0.3])
+            self.light_component.set_specular_color([0.7, 0.3, 0.3])
+            self.light_component.set_ambient_color([0.1, 0.0, 0.0])
             self.state = 0
         elif new_state == 1:
-            self.light_component.set_diffuse_color([0.0, 1.0, 0.0])
+            self.light_component.set_diffuse_color([0.3, 0.7, 0.3])
+            self.light_component.set_specular_color([0.3, 0.7, 0.3])
+            self.light_component.set_ambient_color([0.0, 0.1, 0.0])
             self.state = 1
         elif new_state == 2:
-            self.light_component.set_diffuse_color([0.0, 0.0, 1.0])
+            self.light_component.set_diffuse_color([0.3, 0.3, 0.7])
+            self.light_component.set_specular_color([0.3, 0.3, 0.7])
+            self.light_component.set_ambient_color([0.0, 0.0, 0.1])
             self.state = 2
         elif new_state == 3:
             self.light_component.set_diffuse_color([1.0, 1.0, 1.0])
+            self.light_component.set_specular_color([1.0, 1.0, 1.0])
+            self.light_component.set_ambient_color([0.1, 0.1, 0.1])
             self.state = 3
